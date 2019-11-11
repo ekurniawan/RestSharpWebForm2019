3fc1d5f1e5ac76bdc752c7b4c2210200a7a896ab
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-11-11 13:53:16 +0700

    Add project files.

---
+++ WebFormClient/StudentsForm.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="StudentsForm.aspx.cs" Inherits="WebFormClient.StudentsForm" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        
+        <div class="col-md-6">
+            <h2>Daftar Mahasiswa</h2>
+            <asp:GridView runat="server" ID="gvStudents" CssClass="table table-striped">
+            </asp:GridView>
+            <asp:Label ID="lblKet" runat="server" />
+        </div>
+        <div class="col-md-6">
+            <h2>Insert Edit Student</h2>
+            <div class="form-group">
+                <label for="txtID">ID :</label>
+                <asp:TextBox runat="server" ID="txtID" CssClass="form-control" />
+            </div>
+            <div class="form-group">
+                <label for="txtFirstName">First Name :</label>
+                <asp:TextBox runat="server" ID="txtFirstName" CssClass="form-control" />
+            </div>
+            <div class="form-group">
+                <label for="txtLastName">Last Name :</label>
+                <asp:TextBox runat="server" ID="txtLastName" CssClass="form-control" />
+            </div>
+            <div class="form-group">
+                <label for="txtAddress">Address :</label>
+                <asp:TextBox runat="server" ID="txtAddress" CssClass="form-control" />
+            </div>
+            <div class="form-group">
+                <label for="txtEnrollment">Enrollment Date :</label>
+                <asp:TextBox runat="server" ID="txtEnrollment" TextMode="Date" 
+                    CssClass="form-control" />
+            </div>
+            <asp:Button Text="Submit" ID="btnSubmit" 
+                CssClass="btn btn-default" runat="server" OnClick="btnSubmit_Click" />
+            <asp:Button Text="Edit" ID="btnEdit" CssClass="btn btn-primary" runat="server" OnClick="btnEdit_Click" />
+        </div>
+    </div>
+</asp:Content>
---
+++ WebFormClient/StudentsForm.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebFormClient.Models;
+using RestSharp;
+
+namespace WebFormClient
+{
+    public partial class StudentsForm : System.Web.UI.Page
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://localhost:44319");
+            var request = new RestRequest("/api/Students", Method.GET);
+            var response = client.Execute<List<Student>>(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                gvStudents.DataSource = response.Data;
+                gvStudents.DataBind();
+            }
+            else
+            {
+                lblKet.Text = "Gagal request data";
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://localhost:44319");
+            var request = new RestRequest("/api/Students", Method.POST){
+                RequestFormat=DataFormat.Json };
+
+            var newStudent = new Student
+            {
+                FirstMidName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                Address = txtAddress.Text,
+                EnrollmentDate = Convert.ToDateTime(txtEnrollment.Text)
+            };
+
+            request.AddJsonBody(newStudent);
+            try
+            {
+                var response = client.Execute(request);
+                gvStudents.DataBind();
+                lblKet.Text = $"Data berhasil ditambah";
+            }
+            catch (Exception ex)
+            {
+                lblKet.Text = $"Data gagal ditambah {ex.Message}";
+            }
+
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            var id = txtID.Text;
+            var client = new RestClient("https://localhost:44319");
+            var request = new RestRequest($"/api/Students/{id}", Method.PUT)
+            {
+                RequestFormat = DataFormat.Json
+            };
+
+            var editStudent = new Student
+            {
+                ID=Convert.ToInt32(txtID.Text),
+                FirstMidName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                Address = txtAddress.Text,
+                EnrollmentDate = Convert.ToDateTime(txtEnrollment.Text)
+            };
+            request.AddJsonBody(editStudent);
+
+            try
+            {
+                var response = client.Execute(request);
+                gvStudents.DataBind();
+                lblKet.Text = $"Data berhasil diedit";
+            }
+            catch (Exception ex)
+            {
+                lblKet.Text = $"Data gagal diedit {ex.Message}";
+            }
+        }
+    }
+}
